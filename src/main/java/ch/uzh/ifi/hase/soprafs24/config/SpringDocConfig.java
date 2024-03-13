afb32bbed7a0f653abb5e2414ef56cfd62d05253
samuelfrnk <93734771+samuelfrnk@users.noreply.github.com>
2024-03-13 18:40:40 +0100

    10 add swagger UI index page specjson (#11)

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/SpringDocConfig.java
@@ -0,0 +1,34 @@
+package ch.uzh.ifi.hase.soprafs24.config;
+
+import org.springdoc.core.SpringDocConfigProperties;
+import org.springdoc.core.SpringDocConfiguration;
+import org.springdoc.core.SpringDocUIConfiguration;
+import org.springdoc.core.SwaggerUiConfigProperties;
+import org.springdoc.core.providers.ObjectMapperProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
+
+@Configuration
+public class SpringDocConfig {
+    @Bean
+    SpringDocConfiguration springDocConfiguration(){
+        return new SpringDocConfiguration();
+    }
+
+    @Bean
+    SpringDocConfigProperties springDocConfigProperties() {
+        return new SpringDocConfigProperties();
+    }
+
+    @Bean
+    ObjectMapperProvider objectMapperProvider(SpringDocConfigProperties springDocConfigProperties){
+        return new ObjectMapperProvider(springDocConfigProperties);
+    }
+
+    @Bean
+    SpringDocUIConfiguration SpringDocUIConfiguration(Optional<SwaggerUiConfigProperties> optionalSwaggerUiConfigProperties){
+        return new SpringDocUIConfiguration(optionalSwaggerUiConfigProperties);
+    }
+}
