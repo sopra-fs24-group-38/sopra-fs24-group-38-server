2d22e03d043452b69f9252a8c5b36f0c4b93ece8
samuelfrnk <samuelandreas.frank@uzh.ch>
2024-03-26 15:53:06 +0100

    readded template for userservice tests #145 #146

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/UserServiceTest.java
@@ -0,0 +1,33 @@
+package ch.uzh.ifi.hase.soprafs24.service;
+
+import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    private User testUser;
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+
+
+}
