6fbaaf2a1f6e74d7f658620c9f014b4952a1e5c3
samuelfrnk <samuelandreas.frank@uzh.ch>
2024-03-27 14:38:53 +0100

    added Lobby Service blue print #145

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/LobbyService.java
@@ -0,0 +1,9 @@
+package ch.uzh.ifi.hase.soprafs24.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class LobbyService {
+}
