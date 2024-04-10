6b5873f9ad1b6fd25c1e74035851ff739b54eaa4
samuelfrnk <samuelandreas.frank@uzh.ch>
2024-04-10 16:18:04 +0200

    TEST for issue #38

--- src/test/java/ch/uzh/ifi/hase/soprafs24/controller/UserControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/UserControllerTest.java
@@ -1,4 +1,65 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerTest {
+    @Value(value = "${local.server.port}")
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    @DisplayName("UserController Test: Issue #38 Bad credentials")
+    @Test
+    public void badCredentialsRaises401() {
+        // Register ...
+        createUser("User1", "password");
+
+        // Try Login with bad credentials (pwd) ...
+        String uri = "http://localhost:" + port + "/users/login";
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("username", "User1");
+        requestBody.put("password", "passwordFalse");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        assertThrows(ResourceAccessException.class, () -> restTemplate.exchange(uri, HttpMethod.POST, requestEntity, String.class));
+    }
+
+    private void createUser(String username, String password) {
+        String uri = "http://localhost:" + port + "/users";
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("username", username);
+        requestBody.put("password", password);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.POST, requestEntity, String.class);
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
 }
