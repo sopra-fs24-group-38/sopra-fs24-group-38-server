7296cc178a6602eeeea6d7ee73c9475ce795f3f4
Elyisha <116687078+Elyisha@users.noreply.github.com>
2024-04-13 18:04:47 +0200

    try with gcp secret, worked on different branch

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/ApiService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/ApiService.java
@@ -2,10 +2,13 @@ package ch.uzh.ifi.hase.soprafs24.service;
 
 import ch.uzh.ifi.hase.soprafs24.constant.LobbyModes;
 import ch.uzh.ifi.hase.soprafs24.model.response.Challenge;
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
+import com.google.cloud.secretmanager.v1.SecretVersionName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,16 +16,19 @@ import java.util.Set;
 @Service
 public class ApiService {
 
-    @Autowired
-    private GCPService gcpService;
-
-    //@Value("${token.api}")
-    //private String token;
-
     public Set<Challenge> generateChallenges(Set<LobbyModes> lobbyModes, int numberRounds) {
 
         Set<Challenge> challenges = new HashSet<>();
-        challenges.add(new Challenge("testchallenge", gcpService.getSecret()));
+        challenges.add(new Challenge("testchallenge", getSecret()));
         return challenges;
+    }
+
+    public String getSecret() {
+        try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
+            SecretVersionName secretVersionName = SecretVersionName.of("sopra-fs24-group-38-server", "TOKEN_API", "latest");
+            return client.accessSecretVersion(secretVersionName).getPayload().getData().toStringUtf8();
+        } catch (IOException e) {
+            throw new RuntimeException("Error retrieving secret from Google Secret Manager", e);
+        }
     }
 }
